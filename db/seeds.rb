782fca849f2248bce186b7dc7e7107c6658eb76b
EC2 Default User <ec2-user@ip-172-31-27-239.ap-northeast-1.compute.internal>
2023-11-30 09:03:37 +0000

    [Modified] Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,24 +3,29 @@ Admin.create!(
   password: 'adminadmin',
 )
 
-Item.create!(
+Item.create!([
+  {
   genre_id: 0,
   name: "aaa",
   description: "bbb",
   price_before_tax: 123,
   is_active: true,
-  )
-Genre.create!(
-  name: "def"
-  )
-Genre.create!(
-  name: "abc"
-  )
-Item.create!(
+  },
+  {
   genre_id: 1,
   name: "ccc",
   description: "ddd",
   price_before_tax: 456,
   is_active: false,
-  )
+  }
+  ])
+Genre.create!([
+  {
+    name: "def"
+  },
+  {
+    name: "abc"
+  }
+  ])
+
 
