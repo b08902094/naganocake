11a2734a60878ae3088316e76acd9d867fca1a78
EC2 Default User <ec2-user@ip-172-31-27-239.ap-northeast-1.compute.internal>
2023-11-30 09:14:15 +0000

    [Fixed] Validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :item_image
+  belongs_to :genre
 
   validates :name, presence: true
   validates :description, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,30 +2,31 @@ Admin.create!(
   email: 'admin@admin',
   password: 'adminadmin',
 )
-
-Item.create!([
-  {
-  genre_id: 0,
-  name: "aaa",
-  description: "bbb",
-  price_before_tax: 123,
-  is_active: true,
-  },
-  {
-  genre_id: 1,
-  name: "ccc",
-  description: "ddd",
-  price_before_tax: 456,
-  is_active: false,
-  }
-  ])
 Genre.create!([
   {
     name: "def"
   },
   {
     name: "abc"
+  },
+  {
+    name: "ghi"
   }
   ])
 
-
+Item.create!([
+  {
+  genre_id: 1,
+  name: "aaa",
+  description: "bbb",
+  price_before_tax: 123,
+  is_active: true,
+  },
+  {
+  genre_id: 2,
+  name: "ccc",
+  description: "ddd",
+  price_before_tax: 456,
+  is_active: false,
+  }
+  ])
