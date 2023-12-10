1b3066063a335da45142b08bf2c9a6ae23e298bd
b08902094 <72532740+b08902094@users.noreply.github.com>
2023-12-10 08:58:12 +0000

    [Finish] Search

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,15 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = @genres.find(params[:genre_id])
+      @items = @genre.items
+    else
+      @items = Item.all
+    end
+    @items_count = @items.count
   end
+
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
